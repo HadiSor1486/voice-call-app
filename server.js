4fb0636f44f45dfe87e29203aa3f012ac9d99bbf
Hadi <hadidaoud.ha@gmail.com>
2024-11-20 01:15:29 +0200

    not sure

--- server.js
+++ server.js
@@ -1,12 +1,75 @@
-socket.on('offer', ({ offer, room }) => {
-    socket.to(room).emit('offer', { offer });
-  });
-  
-  socket.on('answer', ({ answer, room }) => {
-    socket.to(room).emit('answer', { answer });
-  });
-  
-  socket.on('new-ice-candidate', ({ candidate, room }) => {
-    socket.to(room).emit('new-ice-candidate', { candidate });
-  });
-  
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// Serve static files
+app.use(express.static('public'));
+
+// Rooms data structure
+const rooms = {};
+
+// WebSocket connection handling
+io.on('connection', (socket) => {
+    console.log('New user connected:', socket.id);
+
+    // Handle 'create-room' event
+    socket.on('create-room', (room) => {
+        if (!rooms[room]) {
+            rooms[room] = [];
+        }
+        rooms[room].push(socket.id);
+        socket.join(room);
+        console.log(`Room ${room} created or joined by ${socket.id}`);
+    });
+
+    // Handle 'join-room' event
+    socket.on('join-room', (room) => {
+        if (rooms[room]) {
+            rooms[room].push(socket.id);
+            socket.join(room);
+            socket.to(room).emit('user-joined', { id: socket.id });
+            console.log(`User ${socket.id} joined room ${room}`);
+        } else {
+            socket.emit('error', 'Room does not exist.');
+        }
+    });
+
+    // Handle WebRTC signaling
+    socket.on('offer', ({ offer, room }) => {
+        socket.to(room).emit('offer', { offer, id: socket.id });
+    });
+
+    socket.on('answer', ({ answer, room }) => {
+        socket.to(room).emit('answer', { answer, id: socket.id });
+    });
+
+    socket.on('new-ice-candidate', ({ candidate, room }) => {
+        socket.to(room).emit('new-ice-candidate', { candidate, id: socket.id });
+    });
+
+    // Handle user disconnection
+    socket.on('disconnect', () => {
+        console.log(`User ${socket.id} disconnected`);
+        for (const room in rooms) {
+            const index = rooms[room].indexOf(socket.id);
+            if (index !== -1) {
+                rooms[room].splice(index, 1);
+                socket.to(room).emit('user-left', { id: socket.id });
+                if (rooms[room].length === 0) {
+                    delete rooms[room];
+                }
+                break;
+            }
+        }
+    });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
